587edab51425b865d82b287dbd32c7de7beeab3b
sharman <sharmanfaridz@gmail.com>
2024-11-28 17:22:53 +0800

    add a todo comment for next coding session

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ def main():
     print("Initializing game")
     num_pass, num_fail = pygame.init()
     
-    if num_fail > 0:
-        print(f"num pass : #{num_pass}")
-        print(f"num fail : #{num_fail}")
-        raise Exception("Error initializing pygame module!")
+    #todo num_fail is 1, how do I know which module failed to initialize?
+    
+    # if num_fail > 0:
+    #     print(f"num pass : #{num_pass}")
+    #     print(f"num fail : #{num_fail}")
+    #     raise Exception("Error initializing pygame module!")
+    print(f"pygame running : {pygame.get_init()}")
 
 
 if __name__ == "__main__":
